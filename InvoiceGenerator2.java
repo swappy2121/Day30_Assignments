6ee675195aed0f0ce874a1b85d5520033e39bd5d
swapnil <sopiswarrinku77@gmail.com>
2022-05-26 14:30:15 -0700

    Added : created a invoice generator test case and again passed the test

---
+++ InvoiceGenerator2.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public  class InvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KM = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+
+    public double calculateFare(double distance, int time) {
+
+
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE) {
+            return MINIMUM_FARE;
+        }else{
+            return totalFare;
+        }
+    }
+
+
+}
